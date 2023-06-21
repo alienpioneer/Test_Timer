d30971fe65275179d1fa699777cf6bce4c32a4b1
Alex Alexandrescu <alex.alexandresq@gmail.com>
2023-06-21 22:16:13 +0200

    Add seconds count

--- main.cpp
+++ main.cpp
@@ -11,11 +11,27 @@ int main()
 	Timer timer(std::chrono::milliseconds(3000));
 	timer.start(testFn);
 
+    int previousSecond = 0;
+
     auto start = std::chrono::steady_clock::now();
+
     while (true)
     {
-        if (std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - start).count() > 20000)
+        long long currentTimeDif = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - start).count();
+
+        int currentSecond = currentTimeDif / 1000;
+
+        if (currentTimeDif % 1000 == 0U && currentSecond != previousSecond)
+        {
+            std::cout << currentSecond << std::endl;
+            previousSecond = currentSecond;
+        }
+            
+        if (currentSecond > 20)
+        {
             timer.stop();
+            break;
+        }
     }
     
 	std::cout << "End" << std::endl;
